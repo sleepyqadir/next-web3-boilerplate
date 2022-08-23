54c76856076973b234df2e80235ef61e7c697589
AQadir64 <a.qadir67@outlook.com>
2022-08-23 19:52:14 +0500

    chore: utility functions added

--- util.ts
+++ util.ts
@@ -1,5 +1,6 @@
-import type { BigNumberish } from "@ethersproject/bignumber";
-import { formatUnits } from "@ethersproject/units";
+import type { BigNumberish } from '@ethersproject/bignumber';
+import { formatUnits } from '@ethersproject/units';
+import { networks } from './config';
 
 export function shortenHex(hex: string, length = 4) {
   return `${hex.substring(0, length + 2)}…${hex.substring(
@@ -8,23 +9,23 @@ export function shortenHex(hex: string, length = 4) {
 }
 
 const ETHERSCAN_PREFIXES = {
-  1: "",
-  3: "ropsten.",
-  4: "rinkeby.",
-  5: "goerli.",
-  42: "kovan.",
+  1: '',
+  3: 'ropsten.',
+  4: 'rinkeby.',
+  5: 'goerli.',
+  42: 'kovan.',
 };
 
 export function formatEtherscanLink(
-  type: "Account" | "Transaction",
+  type: 'Account' | 'Transaction',
   data: [number, string]
 ) {
   switch (type) {
-    case "Account": {
+    case 'Account': {
       const [chainId, address] = data;
       return `https://${ETHERSCAN_PREFIXES[chainId]}etherscan.io/address/${address}`;
     }
-    case "Transaction": {
+    case 'Transaction': {
       const [chainId, hash] = data;
       return `https://${ETHERSCAN_PREFIXES[chainId]}etherscan.io/tx/${hash}`;
     }
@@ -36,3 +37,33 @@ export const parseBalance = (
   decimals = 18,
   decimalsToDisplay = 3
 ) => parseFloat(formatUnits(value, decimals)).toFixed(decimalsToDisplay);
+
+export const getNetwork = (id: number) => {
+  return networks[id];
+};
+
+export const isSupportedNetwork = (id: number): boolean => {
+  const networks = {
+    1: false,
+    3: false,
+    4: true,
+    5: false,
+    137: true,
+    80001: true,
+  };
+
+  return networks[id];
+};
+
+export const switchNetwork = (hexId) => {
+  // @ts-ignore
+  window.ethereum.on('chainChanged', () => {
+    window.location.reload();
+  });
+  // @ts-ignore
+  window.ethereum.request({
+    method: 'wallet_switchEthereumChain',
+    params: [{ chainId: hexId }],
+  });
+  return;
+};
